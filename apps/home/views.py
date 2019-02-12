90cbdab97b1fe0495d869819f2810147cc07e9b2
austinmparham <42558200+austinmparham@users.noreply.github.com>
2019-02-12 16:09:47 -0600

    change to home route to spells.html

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,8 +3,11 @@ import urllib as urllib2, json
 from .models import *
 # the index function is called when root is visited
 def index(request):
-	response = "Hello, I am your first request!"
-	return HttpResponse(response)
+	spells = Spell.objects.all()
+	content = {
+	'spells': spells
+	}
+	return render(request, "blu_app/spells.html", content)
 
 def spells(request):
 	spells = Spell.objects.all()
